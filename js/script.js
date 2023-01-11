65ea381fd741095da575d115240319082c79ef0e
YagubMammedov <yagub.m93@gmail.com>
2023-01-11 20:24:12 +0400

    added: section

--- js/script.js
+++ js/script.js
@@ -1,14 +1,56 @@
-const swiper = new Swiper('.swiper', {
+function sw() {
+  const swiper = new Swiper('.swiper', {
     
-  loop: true,
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  }); 
+}
 
-  pagination: {
-    el: '.swiper-pagination',
-  },
+function sw2() {
+  const swiper = new Swiper('.swiper', {
+    
+    loop: true,
 
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.features-menu__img-next',
+      prevEl: '.features-menu__img-prev',
+    },   
+  });
+}
 
-}); 
+function sw3() {
+  const swiper = new Swiper('.swiper', {
+    
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.review-menu__img-next',
+      prevEl: '.review-menu__img-prev',
+    },   
+  });
+}
+
+
+
+sw()
+sw2()
+sw3()
